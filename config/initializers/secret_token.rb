9a9deb89d6a6947e1d20b208b294be58612d3606
Rasool Khan <rasoolkhan.patan@gmail.com>
2016-01-07 14:23:24 +0530

    spree core files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NattyTest::Application.config.secret_key_base = 'c0c45e0871abab6f99d1c4bf1971a5fc674fe109d1520577f124ce38e1b374a73266efe9d00940cfc3e6dec1962687378dae8e71f4086cf96e412da5eb8834eb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NattyTest::Application.config.session_store :cookie_store, key: '_natty-test_session'
