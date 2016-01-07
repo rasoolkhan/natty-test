44da3f2e7bb666600d39c717980a26876a4b47c6
Rasool Khan <rasoolkhan.patan@gmail.com>
2016-01-07 14:30:39 +0530

    add devise.rb file

---
+++ config/initializers/devise.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
+Devise.secret_key = "f5557e47d0cf5086a2cdb223fda1e72bee423e54bcf40c9bba55167dfda06c13dade1fac88c29b300d71736736d14db324ew"
